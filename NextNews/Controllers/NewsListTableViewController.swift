0bdeccf86ed390485d5557622bcc5f36f7a7d714
coderfox98 <coderfox98@gmail.com>
2019-06-12 16:37:16 +0530

    Completed Application

---
+++ NextNews/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NewsListTableViewController.swift
+//  NextNews
+//
+//  Created by Rishabh Raj on 12/06/19.
+//  Copyright © 2019 Rishabh Raj. All rights reserved.
+//
+
+import Foundation
---
+++ NextNews/Services/Webservice.swift
@@ -0,0 +1,9 @@
+//
+//  Webservice.swift
+//  NextNews
+//
+//  Created by Rishabh Raj on 12/06/19.
+//  Copyright © 2019 Rishabh Raj. All rights reserved.
+//
+
+import Foundation
