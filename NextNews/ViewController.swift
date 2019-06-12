3f256e8b02a96b80439e4e23d5b3f94e52a5fdef
coderfox98 <coderfox98@gmail.com>
2019-06-12 13:07:52 +0530

    Initial Commit

---
+++ NextNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NextNews
+//
+//  Created by Rishabh Raj on 12/06/19.
+//  Copyright © 2019 Rishabh Raj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
